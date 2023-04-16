ce9b0f281955bb2304fb56688657ad9048ffba1a
rsalbergaria <gabrielle.rosane.santos@gmail.com>
2023-04-16 14:52:52 -0300

    adc database and base structure

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# scrap-products
-Backend challeng
+# Scrap Products
+
+Scrap Products is a Python api to receive a product url and return datas about this product;
+
+## Usafe
+
+1 - Run docker to dabatabase
+```bash
+cd mongo 
+docker compose up -d
+```
+
+2 - Run api
+```bash
+cd app 
+pip3 install -r requirements.txt
+python app.py
+```
+
---
+++ app/app.py
@@ -0,0 +1,79 @@
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime, timedelta
+from flask import Flask, request, jsonify
+from pymongo import MongoClient
+
+app = Flask(__name__)
+
+# Connect to MongoDB using the root user and password
+client = MongoClient('mongodb://root:password@localhost:27017/')
+
+# Select the database
+db = client['product_database']
+
+# Select the collection
+collection = db['products']
+
+# Set expiration time for cache (1 hour)
+cache_expiration = timedelta(hours=1)
+
+# Define a function to scrape product data from a URL
+def get_product_data(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    title_element = soup.find('h1', {'class': 'product-name'})
+    title = title_element.get_text() if title_element else None
+
+    image_element = soup.find('img', {'class': 'js-main-image'})
+    image = image_element['src'] if image_element else None
+
+    price_element = soup.find('span', {'class': 'price-current'})
+    price = price_element.get_text() if price_element else None
+
+    description_element = soup.find('div', {'class': 'product-description'})
+    description = description_element.get_text() if description_element else None
+
+    data = {
+        'title': title,
+        'image': image,
+        'price': price,
+        'description': description,
+        'url': url,
+        'timestamp': datetime.utcnow()
+    }
+
+    return data
+
+# Define a function to check if cached data is still valid
+def is_cached_data_valid(data):
+    now = datetime.utcnow()
+    timestamp = data.get('timestamp')
+    if not timestamp:
+        return False
+    return (now - timestamp) < cache_expiration
+
+# Define the API endpoint
+@app.route('/products', methods=['POST'])
+def get_product():
+    data = request.get_json()
+    url = data.get('url')
+    if not url:
+        return jsonify({'error': 'No URL provided.'}), 400
+    
+    # Check if product data is already in cache
+    cached_data = collection.find_one({'url': url})
+    if cached_data and is_cached_data_valid(cached_data):
+        return jsonify(cached_data)
+    
+    # Scrape product data from URL
+    product_data = get_product_data(url)
+    
+    # Cache product data in database
+    collection.replace_one({'url': url}, product_data, upsert=True)
+    
+    return jsonify(product_data)
+
+if __name__ == '__main__':
+    app.run()
